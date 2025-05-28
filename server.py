f63bd7690e0db3701d478859e7a9a1f39327e8c4
almuneraca2009 <35903929+almuneraca2009@users.noreply.github.com>
2025-05-27 23:43:46 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 app = Flask(__name__)
+
+# Variable global para guardar el estado
+estado_led = False
 
 @app.route("/")
 def index():
@@ -7,9 +10,14 @@ def index():
 
 @app.route("/api/led", methods=["POST"])
 def controlar_led():
-    estado = request.json.get("estado")
-    print("Estado recibido:", estado)
+    global estado_led
+    estado_led = request.json.get("estado", False)
+    print("Estado recibido:", estado_led)
     return {"ok": True}
+
+@app.route("/api/estado", methods=["GET"])
+def obtener_estado():
+    return jsonify({"estado": estado_led})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
